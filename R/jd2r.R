4fc308a1d02b5716861e79da0efc7d37e077b39e
AQlT <alain.quartier@yahoo.fr>
2022-03-20 11:53:59 +0100

    export utility functions and put them all in one .Rd to avoid ::: in other packages double jd2r function removed

--- R/jd2r.R
+++ R/jd2r.R
@@ -1,5 +1,20 @@
 #' @include utils.R
+NULL
+#> NULL
 
+
+#' Java Utility Functions
+#'
+#' These functions are used in all JDemetra+ 3.0 packages to easily interact between R and Java objects.
+#'
+#' @param jtest,s,period,startYear,startPeriod,length,ldt,dt,jparams,start,end,y,m,d,year,month,day,type,p,code,prefix,r,span,rspan parameters.
+#'
+#' @name jd3_utilities
+NULL
+#> NULL
+
+#' @export
+#' @rdname jd3_utilities
 jd2r_test<-function(jtest){
   if (is.jnull(jtest))
     return (NULL)
@@ -11,6 +26,8 @@ jd2r_test<-function(jtest){
   }
 }
 
+#' @export
+#' @rdname jd3_utilities
 ts_r2jd<-function(s){
   if (is.null(s)){
     return (NULL)
@@ -21,12 +38,15 @@ ts_r2jd<-function(s){
          as.integer(freq), as.integer(start[1]), as.integer(start[2]), as.double(s))
 }
 
+#' @export
+#' @rdname jd3_utilities
 tsdomain_r2jd<-function(period, startYear, startPeriod, length){
   .jcall("demetra/timeseries/r/TsUtility", "Ldemetra/timeseries/TsDomain;", "of",
          as.integer(period), as.integer(startYear), as.integer(startPeriod), as.integer(length))
 }
 
-
+#' @export
+#' @rdname jd3_utilities
 ts_jd2r<-function(s){
   if (is.null(s)){
     return (NULL)
@@ -39,6 +59,8 @@ ts_jd2r<-function(s){
   ts(x,start=pstart[2:3], frequency=pstart[1])
 }
 
+#' @export
+#' @rdname jd3_utilities
 matrix_jd2r<-function(s){
   if (is.jnull(s)){
     return (NULL)
@@ -49,6 +71,8 @@ matrix_jd2r<-function(s){
   return (array(d, dim=c(nr, nc)))
 }
 
+#' @export
+#' @rdname jd3_utilities
 matrix_r2jd<-function(s){
   if (is.null(s))
     return (.jnull("demetra/math/matrices/Matrix"))
@@ -59,17 +83,20 @@ matrix_r2jd<-function(s){
   return (.jcall("demetra/math/matrices/Matrix","Ldemetra/math/matrices/Matrix;", "of", as.double(s), as.integer(sdim[1]), as.integer(sdim[2])))
 }
 
-jd2r_test<-function(jtest){
-  if (is.jnull(jtest))
-    return (NULL)
-  else{
-    desc<-.jcall(jtest, "S", "getDescription")
-    val<-.jcall(jtest, "D", "getValue")
-    pval<-.jcall(jtest, "D", "getPvalue")
-    return (list(value=val, pvalue=pval, distribution=desc))
-  }
-}
+# Delete this duplicated function?
+# jd2r_test<-function(jtest){
+#   if (is.jnull(jtest))
+#     return (NULL)
+#   else{
+#     desc<-.jcall(jtest, "S", "getDescription")
+#     val<-.jcall(jtest, "D", "getValue")
+#     pval<-.jcall(jtest, "D", "getPvalue")
+#     return (list(value=val, pvalue=pval, distribution=desc))
+#   }
+# }
 
+#' @export
+#' @rdname jd3_utilities
 j2r_ldt<-function(ldt){
   if (is.jnull(ldt))
     return (NULL)
@@ -77,23 +104,30 @@ j2r_ldt<-function(ldt){
   return (as.Date(.jcall(dt, "S", "toString")))
 }
 
+#' @export
+#' @rdname jd3_utilities
 j2r_dt<-function(dt){
   if (is.jnull(dt))
     return (NULL)
   return (as.Date(.jcall(dt, "S", "toString")))
 }
 
+#' @export
+#' @rdname jd3_utilities
 r2j_dt<-function(dt){
   jdt<-.jnew("java/lang/String", as.character(dt))
   return (.jcall("java/time/LocalDate", "Ljava/time/LocalDate;", "parse", .jcast(jdt, "java/lang/CharSequence")))
 }
 
+#' @export
+#' @rdname jd3_utilities
 r2j_ldt<-function(dt){
   jdt<-r2j_dt(dt)
   return (.jcall(jdt, "Ljava/time/LocalDateTime;", "atStartOfDay"))
 }
 
-
+#' @rdname jd3_utilities
+#' @export
 jd2r_parameters <- function(jparams){
   if (is.jnull(jparams))
     return(NULL)
@@ -111,6 +145,8 @@ jd2r_parameters <- function(jparams){
   data_param
 }
 
+#' @export
+#' @rdname jd3_utilities
 jdomain<-function(period, start, end){
   if (period == 0)return (.jnull("demetra/timeseries/TsDomain"))
   n<-period*(end[1]-start[1])+end[2]-start[2]
@@ -118,4 +154,3 @@ jdomain<-function(period, start, end){
                , as.integer(period), as.integer(start[1]), as.integer(start[2]), as.integer(n))
   return (jdom)
 }
-
