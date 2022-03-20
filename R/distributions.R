aada37da398945e98173824fc21697dd84307845
AQlT <alain.quartier@yahoo.fr>
2022-03-20 11:51:30 +0100

    distribution documentation

--- R/distributions.R
+++ R/distributions.R
@@ -1,209 +1,133 @@
-#' Title
+#' The Student Distribution
 #'
-#' @param df
-#' @param n
+#' Density, (cumulative) distribution function and random generation for Student distribution.
 #'
-#' @return
-#' @export
+#' @param df degrees of freedom.
+#' @param n number of observations.
+#' @param x vector of quantiles.
 #'
-#' @examples
-#' # T with 2 degrees of freedom. 100 randoms
-#' z<-randomsT(2, 100)
-randomsT<-function(df, n){
-  .jcall("demetra/stats/r/Distributions", "[D", "randomsT", df, as.integer(n))
-}
-
-#' Title
-#'
-#' @param df
-#' @param x
-#'
-#' @return
-#' @export
 #'
 #' @examples
 #' # T with 2 degrees of freedom.
 #' z<-densityT(2, .01*seq(-100, 100, 1))
+#' # T with 2 degrees of freedom. 100 randoms
+#' z<-randomsT(2, 100)
+#' @name normaldistribution
+#' @rdname normaldistribution
+#' @export
+randomsT<-function(df, n){
+  .jcall("demetra/stats/r/Distributions", "[D", "randomsT", df, as.integer(n))
+}
+
+#' @rdname normaldistribution
+#' @export
 densityT<-function(df, x){
   .jcall("demetra/stats/r/Distributions", "[D", "densityT", df, .jarray(as.numeric(x)))
 }
 
-#' Title
-#'
-#' @param df
-#' @param x
-#'
-#' @return
+#' @rdname normaldistribution
 #' @export
-#'
-#' @examples
 cdfT<-function(df, x){
   .jcall("demetra/stats/r/Distributions", "[D", "cdfT", df, .jarray(as.numeric(x)))
 }
 
-#' Title
+#' The Chi-Squared Distribution
 #'
-#' @param df
-#' @param n
+#' Density, (cumulative) distribution function and random generation for chi-squared distribution.
 #'
-#' @return
+#' @inheritParams normaldistribution
+#'
+#' @name chi2distribution
+#' @rdname chi2distribution
 #' @export
-#'
-#' @examples
 randomsChi2<-function(df, n){
   .jcall("demetra/stats/r/Distributions", "[D", "randomsChi2", df, as.integer(n))
 }
 
-#' Title
-#'
-#' @param df
-#' @param x
-#'
-#' @return
+#' @rdname chi2distribution
 #' @export
-#'
-#' @examples
 densityChi2<-function(df, x){
   .jcall("demetra/stats/r/Distributions", "[D", "densityChi2", df, .jarray(as.numeric(x)))
 }
 
-#' Title
-#'
-#' @param df
-#' @param x
-#'
-#' @return
+#' @rdname chi2distribution
 #' @export
-#'
-#' @examples
 cdfChi2<-function(df, x){
   .jcall("demetra/stats/r/Distributions", "[D", "cdfChi2", df, .jarray(as.numeric(x)))
 }
 
-#' Title
+#' The Gamma Distribution
 #'
-#' @param shape
-#' @param scale
-#' @param n
+#' Density, (cumulative) distribution function and random generation for Gamma distribution.
 #'
-#' @return
+#' @inheritParams normaldistribution
+#' @param shape,scale shape and scale parameters.
+#'
+#' @name gammadistribution
+#' @rdname gammadistribution
 #' @export
-#'
-#' @examples
 randomsGamma<-function(shape, scale, n){
   .jcall("demetra/stats/r/Distributions", "[D", "randomsGamma", shape, scale, as.integer(n))
 }
 
-#' Title
-#'
-#' @param shape
-#' @param scale
-#' @param x
-#'
-#' @return
+#' @rdname gammadistribution
 #' @export
-#'
-#' @examples
 densityGamma<-function(shape, scale, x){
   .jcall("demetra/stats/r/Distributions", "[D", "densityGamma", shape, scale, .jarray(as.numeric(x)))
 }
 
-#' Title
-#'
-#' @param shape
-#' @param scale
-#' @param x
-#'
-#' @return
+#' @rdname gammadistribution
 #' @export
-#'
-#' @examples
 cdfGamma<-function(shape, scale, x){
   .jcall("demetra/stats/r/Distributions", "[D", "cdfGamma", shape, scale, .jarray(as.numeric(x)))
 }
 
-#' Title
+#' The Inverse-Gamma Distribution
 #'
-#' @param shape
-#' @param scale
-#' @param n
+#' Density, (cumulative) distribution function and random generation for inverse-gamma distribution.
 #'
-#' @return
+#' @inheritParams gammadistribution
+#'
+#' @name invgammadistribution
+#' @rdname invgammadistribution
 #' @export
-#'
-#' @examples
 randomsInverseGamma<-function(shape, scale, n){
   .jcall("demetra/stats/r/Distributions", "[D", "randomsInverseGamma", shape, scale, as.integer(n))
 }
 
-#' Title
-#'
-#' @param shape
-#' @param scale
-#' @param x
-#'
-#' @return
+#' @rdname invgammadistribution
 #' @export
-#'
-#' @examples
 densityInverseGamma<-function(shape, scale, x){
   .jcall("demetra/stats/r/Distributions", "[D", "densityInverseGamma", shape, scale, .jarray(as.numeric(x)))
 }
 
-#' Title
-#'
-#' @param shape
-#' @param scale
-#' @param x
-#'
-#' @return
+#' @rdname invgammadistribution
 #' @export
-#'
-#' @examples
 cdfInverseGamma<-function(shape, scale, x){
   .jcall("demetra/stats/r/Distributions", "[D", "cdfInverseGamma", shape, scale, .jarray(as.numeric(x)))
 }
 
-#' Title
+#' The Inverse-Gaussian Distribution
 #'
-#' @param shape
-#' @param scale
-#' @param n
+#' Density, (cumulative) distribution function and random generation for inverse-gaussian distribution.
 #'
-#' @return
+#' @inheritParams gammadistribution
+#'
+#' @name invgaussiandistribution
+#' @rdname invgaussiandistribution
 #' @export
-#'
-#' @examples
 randomsInverseGaussian<-function(shape, scale, n){
   .jcall("demetra/stats/r/Distributions", "[D", "randomsInverseGaussian", shape, scale, as.integer(n))
 }
 
-#' Title
-#'
-#' @param shape
-#' @param scale
-#' @param x
-#'
-#' @return
+#' @rdname invgaussiandistribution
 #' @export
-#'
-#' @examples
 densityInverseGaussian<-function(shape, scale, x){
   .jcall("demetra/stats/r/Distributions", "[D", "densityInverseGaussian", shape, scale, .jarray(as.numeric(x)))
 }
 
-#' Title
-#'
-#' @param shape
-#' @param scale
-#' @param x
-#'
-#' @return
+#' @rdname invgaussiandistribution
 #' @export
-#'
-#' @examples
 cdfInverseGaussian<-function(shape, scale, x){
   .jcall("demetra/stats/r/Distributions", "[D", "cdfInverseGaussian", shape, scale, .jarray(as.numeric(x)))
 }
-
-
