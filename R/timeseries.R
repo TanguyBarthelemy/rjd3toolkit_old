03684ddf89a54f1f5a23691c71e0e760bbe93324
AQlT <alain.quartier@yahoo.fr>
2022-03-20 11:54:48 +0100

    aggregation documentation

--- R/timeseries.R
+++ R/timeseries.R
@@ -3,16 +3,29 @@ NULL
 
 #' Aggregation of time series
 #'
-#' @param s
-#' @param nfreq
-#' @param conversion
-#' @param complete
+#' Makes a frequency change of this series.
 #'
-#' @return
+#' @param s the input time series.
+#' @param nfreq the new frequency. Must be la divisor of the frequency of \code{s}.
+#' @param conversion Aggregation mode: sum (\code{"Sum"}),
+#' average (\code{"Average"}), first observation (\code{"First"}), last observation
+#' (\code{"Last"}), minimum (\code{"Min"}), maximum (\code{"Max"}).
+#' @param complete  boolean indicatif if the observation for a given period in the
+#' new series is set not computed missing if some data in the original series are missing.
+#'
+#' @return A new time series of frequency \code{nfreq}.
 #' @export
 #'
 #' @examples
-aggregate<-function(s, nfreq=1, conversion="Sum", complete=TRUE){
+#' s = ABS$X0.2.09.10.M
+#' # Annual sum
+#' aggregate(s, nfreq = 1, conversion = "Sum")
+#' # Quaterly mean
+#' aggregate(s, nfreq = 4, conversion = "Average")
+aggregate<-function(s, nfreq=1,
+                    conversion=c("Sum", "Average", "First", "Last", "Min", "Max"),
+                    complete=TRUE){
+  conversion <- match.arg(conversion)
   if (is.null(s)){
     return (NULL)
   }
