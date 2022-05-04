318549aaba3da33242bac255bcb949cfdb7e8c3a
palatej <jean.palate@nbb.be>
2022-05-04 16:29:25 +0200

    Upgrade to new java libraries

---
+++ R/calendarts.R
@@ -0,0 +1,24 @@
+#' Title
+#'
+#' @param calendarobs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' obs<-list(
+#' list(start=as.Date("1980-01-01"), end=as.Date("1999-12-31"), value=2000),
+#' list(start=as.Date("2000-01-01"), end=as.Date("2010-01-01"), value=1000)
+#' )
+#' jobj<-r2jd_calendarts(obs)
+r2jd_calendarts<-function(calendarobs){
+  if (is.null(calendarobs) || !is.list(calendarobs)){
+    return (NULL)
+  }
+  starts<-sapply(calendarobs, function(z){as.character(z$start)})
+  ends<-sapply(calendarobs, function(z){as.character(z$end)})
+  values<-sapply(calendarobs, function(z){as.numeric(z$value)})
+  jts<-.jcall("demetra/timeseries/r/TsUtility", "Ldemetra/timeseries/CalendarTimeSeries;", "of",
+        .jarray(starts, "Ljava/lang/String;"), .jarray(ends, "Ljava/lang/String;"), .jarray(values))
+  return (jts)
+}
